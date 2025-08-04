d8b48474fa6244a27d73596600e7f0408a277fb6
miguedevp <angelrr.ti22@utsjr.edu.mx>
2025-08-03 21:12:49 -0600

    Feat: SearchInputComponent componente reutiilizable | viñetas en MenuComponent + CSS

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-input',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.css'
 })
 export class SearchInputComponent {
-
+@Input() placeholder: string = 'Buscar...';
+faSearch = faSearch;
 }
--- src/app/pages/reports/money-report/money-report.component.ts
+++ src/app/pages/reports/money-report/money-report.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SearchInputComponent } from '../../../components/search-input/search-input.component';
 
 @Component({
   selector: 'app-money-report',
-  imports: [],
+  imports: [FormsModule, CommonModule, SearchInputComponent],
   templateUrl: './money-report.component.html',
   styleUrl: './money-report.component.css'
 })
