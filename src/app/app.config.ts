cb59653b863ca59ab0eb4edc4b6d6e447b12d753
miguedevp <angelrr.ti22@utsjr.edu.mx>
2025-11-24 11:49:45 -0600

    Refactor: Refactorizacion CSS Y diseño

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 export function initializeIconLibrary(library: FaIconLibrary) {
   library.addIconPacks(fas); 
 }
@@ -22,6 +23,9 @@ export const appConfig: ApplicationConfig = {
         initializeIconLibrary(library);
         return library;
       }
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
